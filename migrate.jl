837cb5f58cc4f35ced10c7fff22454a4f0f84104
marcosrdac <marcosrdac@gmail.com>
2020-09-03 14:40:01 -0300

    modified migrated.jl to work with new signal scheme

--- migrate.jl
+++ migrate.jl
@@ -1,8 +1,5 @@
 #!/usr/bin/env julia
-
 using Base.Threads
-
-macro println() :(println()) end
 
 "including \"propagate_module\", using Propagate module" |> println
 include("./propagate_module.jl")
@@ -14,38 +11,39 @@ include("./parameters.jl")
 "defining grid" |> println
 grid = FDM_Grid(Δz, Δx, Δt, NZ, NX, NT)
 
-"defining signal" |> println
-position = sourceposition(array, NZ, NX)
-source_signature = discarray(source_signature_file)
-signal = Signal1D(source_signature, position)
-
 "defining velocity model" |> println
 v = discarray(v_file)
 
 "defining initial pressure field" |> println
 P0 = zero(v)
 
-@println
+"defining signal" |> println
+source_signature = discarray(source_signature_file)
+(sz, sx) = sourceposition(array, NZ, NX)
+signal = signal1d(sz, sx, source_signature)
+
+println()
 
 "source signal propagation" |> println
 @time propagate_save(grid, P0, v, signal;
                      filename=P_file, stencil_order=stencil_order)
 
 
-@println
+println()
 
 
 "source signal direct wave propagation" |> println
 @time propagate_save_seis(grid, P0, v, signal;
-                          filename=direct_seis_file, direct_only=true, stencil_order=stencil_order)
+                          filename=direct_seis_file, stencil_order=stencil_order,
+                          direct_only=true)
 
 
-@println
+println()
 
 
 "reverse wave propagation" |> println
 begin
-    seis = slice_seismogram(discarray(P_file))
+    seis = P2seis(discarray(P_file))
     direct_seis = discarray(direct_seis_file)
 
     # removing direct_wave from seismogram
@@ -53,12 +51,12 @@ begin
         seis[I] -= direct_seis[I]
     end
 
-    seis_signal = Signal2D(seis, CartesianIndex(1, 1))
+    seis_signals = seis2signals(seis)
 end
-@time propagate_2d_save(grid, P0, v, seis_signal; filename=reversed_P_file, stencil_order=stencil_order)
+@time propagate_save(grid, P0, v, seis_signals; filename=reversed_P_file, stencil_order=stencil_order)
 
 
-@println
+println()
 
 
 "applying image condition" |> println
