e3ae2041a504edda78eca8a96404ef75c8a163c8
marcosrdac <marcosrdac@gmail.com>
2020-09-13 03:27:21 -0300

    easier to use 'animate.py'

--- animate.py
+++ animate.py
@@ -1,55 +1,71 @@
 #!/usr/bin/env python3
 
-from os.path import join
+from os.path import join, basename, splitext
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from src.discarrays import discarray
 
 
-def animate_snaps(in_fn, out_fn, order='C'):
+def animate_snaps(in_fn, out_fn, order='C',
+                  norm="frameabsmax",
+                  timestep=30, interval=60):
     '''
     Saves a [ny,nx,nt] array time snaps as a gif.
+    Norm can be:
+        - "fullminmax";
+        - "frameminmax";
+        - "fullabsmax";
+        - "frameabsmax";
+        - (vmin, vmax);
     '''
-    # opening read-only file as IO
-    arr = discarray(in_fn, "r", order=order)
-    nz, nx, nt = arr.shape
-
     fig = plt.figure(figsize=(3, 3))
     ax = fig.add_axes([0, 0, 1, 1])
 
-    cur_P = arr[:, :, 0]
-    im = ax.imshow(cur_P, aspect='auto', vmin=-.1, vmax=.1)
+    arr = discarray(in_fn, "r", order=order)
+    nz, nx, nt = arr.shape
+    frame = arr[:, :, 0]
 
-    time_sep = 30
+    im = ax.imshow(frame, aspect='auto')
 
-    def update(t):
-        cur_P = arr[:, :, t]
-        im.set_data(cur_P)
-        # NORMALIZATION METHODS
-        #   1. NONE: comment every other method
-        #
-        # min, max: useful for methods 2 and 3
-        vmin, vmax = np.min(cur_P), np.max(cur_P)
-        #   2. min=min, max=max
-        # im.set_clim(vmin=vmin, vmax=vmax)
-        #
-        #   3. min=-maxabs, max=+maxabs
-        maxabs = np.max(np.abs([vmin, vmax]))
-        im.set_clim(vmin=-maxabs, vmax=maxabs)
+    vmin = vmax = None
+    if norm is not None:
+        if "full" in norm:
+            vmin, vmax = np.min(arr), np.max(arr)
+        elif "frame" in norm:
+            vmin, vmax = np.min(frame), np.max(frame)
+        elif isinstance(norm, tuple):
+            vmin, vmax = norm
+
+        if "absmax" in norm:
+            absmax = np.max(np.abs([vmin, vmax]))
+            vmin, vmax = -absmax, absmax
+        im.set_clim(vmin, vmax)
+
+    def update(t, vmin, vmax):
+        frame = arr[:, :, t]
+        im.set_data(frame)
+        if norm is not None:
+            if "frame" in norm:
+                vmin, vmax = np.min(frame), np.max(frame)
+                if "absmax" in norm:
+                    absmax = np.max(np.abs([vmin, vmax]))
+                    vmin, vmax = -absmax, absmax
+            im.set_clim(vmin=vmin, vmax=vmax)
         return(ax)
 
-    anim = FuncAnimation(fig, update, frames=np.arange(
-        1, nt, time_sep), interval=60)
+    anim = FuncAnimation(fig, update, frames=np.arange(1, nt, timestep),
+                         interval=interval, fargs=(vmin, vmax))
     anim.save(out_fn, dpi=80, writer='imagemagick')
     # plt.show()
 
 
 if __name__ == '__main__':
     folder = "/mnt/hdd/home/tmp/awp_data"
+    Pfile = join(folder, "P.bin")
 
-    animate_snaps(join(folder, "P.bin"),
-                  "animations/P.gif", order='F')
+    animate_snaps((f := Pfile),
+                  f"animations/{splitext(basename(f))[0]}.gif", order='F')
 
     # animate_snaps(join(folder, "reversed_P.bin"),
     #              "animations/reversed_P.gif", order='F')
